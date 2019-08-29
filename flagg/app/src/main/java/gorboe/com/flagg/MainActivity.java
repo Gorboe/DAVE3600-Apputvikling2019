72b9599ef9c085073638fb5a2cb4603ccdeedbc1
Gorboe <31539445+Gorboe@users.noreply.github.com>
2019-08-29 14:02:57 +0200

    added buttonimages

--- flagg/app/src/main/java/gorboe/com/flagg/MainActivity.java
+++ flagg/app/src/main/java/gorboe/com/flagg/MainActivity.java
@@ -1,17 +1,23 @@
 package gorboe.com.flagg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.ConfigurationCompat;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Locale locale;
+    TextView temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnNorsk = findViewById(R.id.btnNorsk);
         Button btnTysk = findViewById(R.id.btnTysk);
+        temp = findViewById(R.id.temp);
+
+
+
+        //temp.setText(locale.getLanguage());
 
         btnNorsk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 System.out.println("CHANGE TO NO");
-                setLocale("no");
+                setLocale("nb");
+                recreate();
             }
         });
 
@@ -32,18 +44,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 System.out.println("CHANGE TO DE");
+                setLocale("de");
+                recreate();
             }
         });
     }
 
     private void setLocale(String lang){
-        Locale locale = new Locale(lang);
+        locale = new Locale(lang);
         Locale.setDefault(locale);
         Configuration config = new Configuration();
         config.locale = locale;
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
-        editor.putString("My_Lang", lang);
-        editor.apply();
+
+        //temp
+        //locale = ConfigurationCompat.getLocales(Resources.getSystem().getConfiguration()).get(0); //current language on phone
+        temp.setText(locale.getLanguage());
     }
 }
