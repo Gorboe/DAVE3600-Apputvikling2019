d0ae35fa922912e34ea7c8c2c6af77f7a2a0785c
Gorboe <31539445+Gorboe@users.noreply.github.com>
2019-11-19 10:09:04 +0100

    removed unused stuff and fixed layout

--- s319482mappe3/app/src/main/java/gorboe/com/s319482mappe3/MainActivity.java
+++ s319482mappe3/app/src/main/java/gorboe/com/s319482mappe3/MainActivity.java
@@ -1,14 +1,8 @@
 package gorboe.com.s319482mappe3;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -16,22 +10,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
-    private EditText input;
-    private TextView output;
     private TextView xcord;
     private TextView ycord;
-    private Server task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +23,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         MapFragment mapFragment = (MapFragment) getFragmentManager() .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        output = findViewById(R.id.output);
-        input = findViewById(R.id.input);
         xcord = findViewById(R.id.xcord);
         ycord = findViewById(R.id.ycord);
 
-        
+
         //task = new Server();
         /*try{
             String result = task.execute("http://student.cs.hioa.no/~s319482/jsonout.php").get();
@@ -63,8 +44,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapClick(LatLng latLng) {
-        String xcoordinates = latLng.longitude + "";
-        String ycoordinates = latLng.latitude + "";
+        String xcoordinates = "x: " + latLng.longitude + "";
+        String ycoordinates = "y: " + latLng.latitude + "";
         xcord.setText(xcoordinates);
         ycord.setText(ycoordinates);
     }
@@ -73,16 +54,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setOnMapClickListener(this);
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(-45, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    public void sendData(View view) {
-        String text = input.getText().toString();
-        task = new Server();
-        task.execute(
-                "http://student.cs.hioa.no/~s319482/jsonin.php/?Name=" + text,
-                "http://student.cs.hioa.no/~s319482/jsonout.php");
     }
 }
